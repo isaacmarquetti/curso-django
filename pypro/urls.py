46a708e3e2da435833490f5792c9a287c31ef32e
Isaac Marquetti <isaac.marquetti@gmail.com>
2021-12-14 21:46:03 -0300

    Criando infra de testes com pytest-django
    close #9

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 from pypro.base import views
-from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
