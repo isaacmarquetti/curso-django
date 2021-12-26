1e1aa6e8e9663036b0cba176b85ca75b76a7aff7
Isaac Marquetti <isaac.marquetti@gmail.com>
2021-12-26 15:14:10 -0300

    Update settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -147,7 +147,6 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 # -----------------------------------------------------
 
 if AWS_ACCESS_KEY_ID:
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
