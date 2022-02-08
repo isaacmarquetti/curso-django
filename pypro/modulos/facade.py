e811837bfde398863addd1c3d9063fd3621b5486
Isaac Marquetti <isaac.marquetti@gmail.com>
2022-02-07 21:28:21 -0300

    App Modulos e Banco de Dados

---
+++ pypro/modulos/facade.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from pypro.modulos.models import Modulo
+
+
+def listar_modulos_ordenados() -> List[Modulo]:
+    """
+    Lista módulos ordenados por títulos
+    :return:
+    """
+
+    return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
+
+
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
+    return list(modulo.aula_set.order_by('order').all())
